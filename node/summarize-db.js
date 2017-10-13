ea357e2f3d694288580750ba2cbf0126dbfcc3f5
Tony Song <songx823@morris.umn.edu>
2017-10-12 22:31:17 -0500

    Implement description printer

--- node/summarize-db.js
+++ node/summarize-db.js
@@ -15,39 +15,56 @@ connection.connect(function(err){
   }
 });
 
-connection.query('Show databases', function (err,rows,fields) {
-  var desMap = {};
-  var count;
-  // for each db
-  asy.each(rows, function (db, callback1) {
-    desMap[db['Database']] = {};
-    connection.query('Show tables from ' + db['Database'], function (err,rows,fields) {
-      // for each table
-      asy.each(rows, function (table, callback2) {
+// .query(queryString, cb)
+var formatStrings = {};
 
-        desMap[db['Database']][table["Tables_in_" + db['Database']]] = {};
-        connection.query('describe ' + db['Database'] + '.' + table['Tables_in_' + db['Database']], function (err,rows,fields) {
-          desMap[db['Database']][table["Tables_in_" + db['Database']]] = rows;
-        });
-        callback2();
-      }, function (err) {
+function handler() {
+    connection.query("show databases", function(err, rows, fields) {
         if (err) {
-          console.log("Sth is wrong accesing tables");
+            console.log(err);
         } else {
-          console.log("Good");
-          callback1();
+            handleDBs(rows, rows.length);
         }
-      });
     });
-  }, function (err) {
-    if (err) {
-      console.log("Something is wrong accessing db names");
-    } else {
-      console.log("Gucci");
-      connection.end();
-      // Wait until desMap is filled
-    }
-  });
-});
+}
 
-console.log("All done now.");
+function handleDBs(dbs, dbNums) {
+    var dbCount = 0;
+    dbs.forEach(function (db) {
+        connection.query("show tables from " + db["Database"], function(err, rows, fields) {
+            if (err) {
+                console.log(err);
+            } else {
+                dbCount++;
+                handleTables(db, rows, rows.length, dbCount == dbNums);
+            }
+        });
+    });
+}
+
+function handleTables(db, tables, tableNums, dbReady) {
+    var tableCount = 0;
+    tables.forEach(function (table) {
+        connection.query("describe " + db["Database"] + '.' + table["Tables_in_"+db["Database"]], function(err, rows, fields) {
+            if (err) {
+                console.log(err);
+            } else {
+                tableCount++;
+                handleDes(db, table, rows);
+                if (dbReady && tableCount == tableNums) {
+                    connection.end();
+                }
+            }
+        });
+    });
+}
+
+function handleDes(db, table, des) {
+    console.log(db);
+    console.log(table);
+    console.log(des);
+}
+
+handler();
+
+console.log("End");
